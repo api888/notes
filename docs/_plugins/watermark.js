7ae4f42b4ec62fcad7ed9a5029b43cffcced3b00
api888 <59526352+api888@users.noreply.github.com>
2022-07-09 13:27:49 +0800

    Create watermark.js

--- docs/_plugins/watermark.js
+++ docs/_plugins/watermark.js
@@ -1,14 +1,17 @@
-function watermark(t1, t2, t3) {
-    let allMark = document.querySelectorAll(".mark_div");
+function watermark() {
+    let allMark = document.getElementsByClassName("mark_div");
     if (allMark) {
-        allMark.forEach((item) => {
-            document.removeChild(item);
-        });
+        for (let i = allMark.length - 1; i >= 0; i--) {
+            if (allMark[i] != null) {
+                allMark[i].parentNode.removeChild(allMark[i]);
+            }
+
+        }
     }
     var maxWidth = document.body.clientWidth;
     var maxHeight = document.body.clientHeight;
     var intervalWidth = 150;    //间隔宽度
-    var intervalheight = 150;   //间隔高度
+    var intervalheight = 120;   //间隔高度
     var rowNumber = maxWidth / intervalWidth;    //横向个数
     var coumnNumber = maxHeight / intervalheight;   //纵向个数
 
@@ -19,7 +22,7 @@ function watermark(t1, t2, t3) {
         watermark_fontsize: '15px', //水印字体大小
         watermark_font: '微软雅黑', //水印字体
         watermark_width: 150, //水印宽度
-        watermark_height: 80, //水印长度
+        // watermark_height: 80, //水印长度
         watermark_angle: 15 //水印倾斜度数
     };
 
@@ -31,16 +34,9 @@ function watermark(t1, t2, t3) {
             var mark_div = document.createElement('div');
             mark_div.id = 'mark_div' + i + j;
             mark_div.className = 'mark_div';
-            ///三个节点
             var span0 = document.createElement('div');
-            span0.appendChild(document.createTextNode(t1));
-            var span1 = document.createElement('div');
-            span1.appendChild(document.createTextNode(t2));
-            var span2 = document.createElement('div');
-            span2.appendChild(document.createTextNode(t3));
+            span0.appendChild(document.createTextNode('api888 | hbb'));
             mark_div.appendChild(span0);
-            mark_div.appendChild(span1);
-            mark_div.appendChild(span2);
             //设置水印div倾斜显示
             mark_div.style.webkitTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
             mark_div.style.MozTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
@@ -48,7 +44,7 @@ function watermark(t1, t2, t3) {
             mark_div.style.OTransform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
             mark_div.style.transform = "rotate(-" + defaultSettings.watermark_angle + "deg)";
             mark_div.style.visibility = "";
-            mark_div.style.position = "absolute";
+            mark_div.style.position = "fixed";
             mark_div.style.left = x + 'px';
             mark_div.style.top = y + 'px';
             mark_div.style.overflow = "hidden";
@@ -82,4 +78,4 @@ const debounce = (fn, delay) => {
 const cancalDebounce = debounce(watermark, 500);
 
 window.addEventListener('resize', cancalDebounce);
-watermark('api888 | hbb', '', '');
+watermark();
